75e49d3d2c7bd2843e2d6b89133fc4f600f1ffc1
fahad2545 <112535621+fahad2545@users.noreply.github.com>
2022-10-01 09:52:08 +0600

    Add files via upload

---
+++ Lab_1/Lab_Task_1.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <h1>
+            Registration Form
+        </h1>
+        <form action="">
+        <table>
+            <tr>
+                <td>
+                    Student Name : 
+                </td>
+                <td></td>
+                <td></td>
+
+                <td>
+                    <select name="" id="">
+                        <option value="Mss">Mss.</option>
+                        <option value="Mr.">Mr.</option>
+                    </select>
+                    <input type="text" placeholder="Enter Your Name">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Father's Name :
+                </td>
+                <td></td>
+                <td></td>
+
+                <td>
+                    <input type="text" placeholder="Enter Your father's Name" name="fname">
+                </td>
+            </tr>
+            <tr>
+                <td>Mother's Name :</td>
+                <td></td>
+                <td></td>
+
+                <td>
+                    <input type="text" placeholder="Enter Your Mother's Name" name="mname">
+                </td>
+            </tr>
+            <tr>
+                <td>Date Of Birth :</td>
+                <td></td>
+                <td></td>
+
+                <td><input type="date" name="date"></td>
+            </tr>
+            <tr>
+                <td>
+                    Gender
+                </td>
+                <td></td>
+                <td></td>
+
+                <td><input type="radio" name="gender" value="male">Male
+                <input type="radio" name="gender" value="female">Female</td>
+            </tr>
+            <tr>
+                <td>Email :</td>
+                <td></td>
+                <td></td>
+
+                <td><input type="email" name="email"></td>
+            </tr>
+            <tr>
+                <td>Mobile No : </td>
+                <td></td>
+                <td></td>
+
+                <td><input type="number" name="number"></td>
+            </tr>
+            <tr>
+                <td>Address : </td>
+                <td></td>
+                <td></td>
+
+                <td><textarea name="address" id="" cols="21" rows="7"></textarea></td>
+            </tr>
+            <tr>
+                <td>State :</td>
+                <td></td>
+                <td></td>
+                <td><input type="text" name="state"></td>
+            </tr>
+            <tr>
+                <td>City</td>
+                <td></td>
+                <td></td>
+
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+
+                <td><input type="submit"></td>
+            </tr>
+        </table>
+        </form>
+    </body>
+</html>
